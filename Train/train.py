cc2f20065373558eff6d8b7f13b7ffece249264e
Anurag Parashar Sarmah <90975247+anuragp787@users.noreply.github.com>
2024-03-31 11:02:51 +0530

    Update train.py

--- Train/train.py
+++ Train/train.py
@@ -33,19 +33,6 @@ from urllib.request import urlopen
 from io import BytesIO
 from zipfile import ZipFile
 
-
-def download_and_unzip(url, extract_to="Datasets"):
-    http_response = urlopen(url)
-    zipfile = ZipFile(BytesIO(http_response.read()))
-    zipfile.extractall(path=extract_to)
-
-
-if not os.path.exists(os.path.join("Datasets", "captcha_images_v2")):
-    download_and_unzip(
-        "https://github.com/AakashKumarNain/CaptchaCracker/raw/master/captcha_images_v2.zip",
-        extract_to="Datasets",
-    )
-
 # Create a list of all the images and labels in the dataset
 dataset, vocab, max_len = [], set(), 0
 captcha_path = os.path.join("Datasets", "captcha_images_v2")
